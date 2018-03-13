91efa6e44b07879ecdaff2d6d2b20d6b77476bac
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2018-03-13 10:38:28 -0600

    Arbol

---
+++ lab3/lab3/Models/Country.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace lab3.Models
+{
+    public class Country
+    {
+        [Key]
+
+        public int NoPartido { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El numero de partido es requerido")]
+        [Display(Name = "Numero de Partido")]
+
+        public DateTime Fecha { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El numero de partido es requerido")]
+        [Display(Name = "Fecha de Partido")]
+
+        public string Grupo { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El grupo es requerido")]
+        [StringLength(1, ErrorMessage = "El grupo debe de ser de solo una letra")]
+        [Display(Name = "Grupo de Partido")]
+
+        public string Pais1 { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Local")]
+
+        public string Pais2 { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Pais")]
+
+        public string Estadio { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Estadio")]
+
+    }
+}
