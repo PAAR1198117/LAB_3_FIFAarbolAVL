1d04f2c1ad988f849461daf351a7510ccc2f51ac
PAAR1198117 <pedrito.ar@gmail.com>
2018-03-14 19:12:35 -0500

    Archivo listo para revision
    Listo para que revisen vistas y controladores (Lectura JSON)

---
+++ lab3/lab3/Clases/Data.cs
@@ -0,0 +1,28 @@
+﻿using lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab3.Clases
+{
+    public class Data
+    {
+        private static Data instance;
+        public static Data Instance
+        {
+            get
+            {
+                if (instance == null) instance = new Data();
+                return instance;
+            }
+        }
+
+        public List<Country> Partidos;
+
+        public Data()
+        {
+            Partidos = new List<Country>();
+        }
+    }
+}
--- lab3/lab3/Models/Country.cs
+++ lab3/lab3/Models/Country.cs
@@ -10,30 +10,35 @@ namespace lab3.Models
     {
         [Key]
 
-        public int NoPartido { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "El numero de partido es requerido")]
         [Display(Name = "Numero de Partido")]
+        public int NoPartido { get; set; }
 
-        public DateTime Fecha { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "El numero de partido es requerido")]
         [Display(Name = "Fecha de Partido")]
+        public DateTime Fecha { get; set; }
 
-        public string Grupo { get; set; }
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "El grupo es requerido")]
         [StringLength(1, ErrorMessage = "El grupo debe de ser de solo una letra")]
         [Display(Name = "Grupo de Partido")]
+        public string Grupo { get; set; }
 
-        public string Pais1 { get; set; }
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
         [Display(Name = "Local")]
+        public string Pais1 { get; set; }
 
-        public string Pais2 { get; set; }
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
         [Display(Name = "Pais")]
+        public string Pais2 { get; set; }
 
-        public string Estadio { get; set; }
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es requerido")]
         [Display(Name = "Estadio")]
+        public string Estadio { get; set; }
+
 
     }
 }
